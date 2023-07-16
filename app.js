04b2aaa0961e79cea974431c21b9c9b5bba5c0e7
Aatif <87392112+Aatifhasan@users.noreply.github.com>
2023-07-16 16:09:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -157,37 +157,3 @@ app.post('/delete', function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
-
-
-
-
-//   Video 242;
-// let today = new Date();
-// let curDay = today.getDay();
-// let day = "";
-
-// switch (curDay) {
-//   case 0:
-//     day = "Sunday";
-//     break;
-//   case 1:
-//     day = "Monday";
-//     break;
-//   case 2:
-//     day = "Tuesday";
-//     break;
-//   case 3:
-//     day = "Wednesday";
-//     break;
-//   case 4:
-//     day = "Thursday";
-//     break;
-//   case 5:
-//     day = "Friday";
-//     break;
-//   case 6:
-//     day = "Saturday";
-//     break;
-//   default:
-//     console.log("Error: current day = "+curDay);
-// }
